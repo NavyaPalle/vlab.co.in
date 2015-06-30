d5cf2660e64261a7990fbf874ce9249e1a0d4602
madhavi <madhavi@localhost.localdomain>
2015-06-30 12:36:51 +0530

    added analytics code

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, jsonify, make_response
+import json
+app = Flask(__name__)
+
+@app.route('/numberofhits',  methods=["GET", "OPTIONS"])
+def numberofhits():
+    json_data = open("data-2014.json")
+    data = json.load(json_data)
+    json_string = json.dumps(data)
+    response = make_response(json_string)
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+    return response
+    #return jsonify(data=json_string)
+
+if __name__ == '__main__':
+    app.run(
+        debug=True
+    )
